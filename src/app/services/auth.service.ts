2e9c693b7693c6853a1621e658926c4507c0e4d3
Will Miller <willmiller629@gmail.com>
2021-01-10 14:39:58 -0500

    auth service w/ other groups help

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { API_URL} from '../app.constant';
+import { map } from 'rxjs/operators';
+
+export const TOKEN = 'token';
+export const AUTHENTICATED_USER = 'authenticatedUser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  username!: string;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  executeJWTAuthenticationService = (username: string, password: string) => this.http.post<any>(
+    `${API_URL}/authenticate`, {
+    username,
+    password
+    }
+  ).pipe(
+    map(
+      response => {
+        sessionStorage.setItem(AUTHENTICATED_USER, username);
+        sessionStorage.setItem(TOKEN, `Bearer ${response.token}`);
+        return response;
+      }
+    )
+  )
+  getAuthenticatedUser = () => {
+    return sessionStorage.getItem(AUTHENTICATED_USER);
+  }
+
+  getAuthenticationToken = () => {
+    if (this.getAuthenticatedUser()){
+      return sessionStorage.getItem(TOKEN);
+    }
+  }
+
+  isUserActive = () => {
+    const user = sessionStorage.getItem(AUTHENTICATED_USER);
+    return !(user === null);
+  }
+
+  logout = () => {
+    sessionStorage.removeItem(AUTHENTICATED_USER);
+    sessionStorage.removeItem(TOKEN);
+  }
+}
