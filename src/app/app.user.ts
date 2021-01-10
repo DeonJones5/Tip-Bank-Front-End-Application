4a08e1a79dfd01f8076ab1e7df6634fa0bb3b8e8
Will Miller <willmiller629@gmail.com>
2021-01-10 14:49:45 -0500

    made a general user with fields from back end

---
+++ src/app/app.user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  userId: number;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  dateOfBirth: string;
+  socialSecurity: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  password: string;
+
+}
